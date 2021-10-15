cdffa11f5e1970c19aba7e8cc017fd16c9467cdb
LesliePHernandez <l18550680@chihuahua2.tecnm.mx>
2021-10-14 18:27:17 -0600

    Proyecto

---
+++ Doorman/app/src/main/java/com/example/doorman/salida.kt
@@ -0,0 +1,11 @@
+package com.example.doorman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class salida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_salida)
+    }
+}
